d768cdcb1000ee1d10ac617cea6afe350169d24f
Siddhartha <avsiddhartha2006@gmail.com>
2026-08-16 10:16:45 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,13 +5,14 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi = max(maxi, sum);
-
+             if(sum >maxi){
+                maxi=sum;
+            }
             if(sum<0){
                 //rset
                 sum=0;
             }
-
+           
         }
         return maxi;
     }
